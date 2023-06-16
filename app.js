9fb5fb59d8e30db9f2d6f98b663e9acc1bc8cd86
tosiron <tosironj@gmail.com>
2023-06-16 08:49:11 +0100

    preparing to host

--- app.js
+++ app.js
@@ -28,7 +28,10 @@ app.use(express.json());
 app.use(helmet())
 app.use(cors())
 app.use(xss())
- 
+app.get('/',(req,res)=>{
+  res.send('Tosirons Job Api')
+}) 
+
 app.use("/api/v1/auth", Authrouter);
 app.use("/api/v1/jobs", authMiddleware, Jobsrouter);
 
